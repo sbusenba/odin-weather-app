4fed5d435a76fbe03e4fe2af601f4e154386782e
Steven Busenbarrick <sbusenba@gmail.com>
2022-03-09 10:48:40 -0800

    Format daycard, add days and today view
    Update CSS to hide the daycard holder div.
    Update daycard to add days of the week, change positioning.
    Add div for today's weather information.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const apiKey = 'ce678a1ab136ef46871798383c6d8d08';
 const input = document.querySelector('#search-text');
 const button = document.querySelector('#search-button');
 const icon = document.querySelector('#icon-display');
-const daycards = document.querySelectorAll('.daycard');
-
+const forcastBar = document.querySelector('#forcast-holder');
+const todayDisplay = document.querySelector('#today-display');
+const display = document.querySelector('#display');
 function downloadImage(response) {
   const iconCode = response.weather[0].icon;
   icon.setAttribute('src', `http://openweathermap.org/img/wn/${iconCode}@4x.png`);
@@ -13,6 +14,12 @@ function setForcast(weatherData) {
   for (let i = 0; i < 8; i += 1) {
     document.querySelector(`#day${i}maxtemp`).textContent = weatherData.daily[i].temp.max;
     document.querySelector(`#day${i}mintemp`).textContent = weatherData.daily[i].temp.min;
+
+    const date = new Date(weatherData.daily[i].dt * 1000);
+    document.querySelector(`#day${i}day`).textContent = date.toLocaleString('en-US', { weekday: 'long' });
+    forcastBar.style.opacity = 1;
+    icon.style.opacity = 1;
+    display.style.opacity = 1;
   }
 }
 
